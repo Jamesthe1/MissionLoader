2db0aa5f8c6247ee5c21c767b0df0c109874272f
Jamesthe1 <jamesthe1sky@gmail.com>
2024-04-11 14:17:12 -0500

    Forget object folder

--- obj/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace MissionLoader
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "MissionLoader";
-        public const string PLUGIN_NAME = "MissionLoader";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
